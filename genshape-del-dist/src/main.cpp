137c45d761242dda4347b4214023810b889e3480
Erik Brisson <ebrisson@gmail.com>
2023-08-05 11:28:22 -0700

    added some cmd line args

--- genshape-del-dist/src/main.cpp
+++ genshape-del-dist/src/main.cpp
@@ -1,9 +1,24 @@
+// File: genshape-del-dist/src/main.cpp
+// Author: Erik Brisson
+// Date created: August 2, 2023
+// Purpose: Create a shape as a boundary representation as follows:
+// Choose a set of points within a 3d rectangle (for now, chosen uniformaly)
+// Tetrahedralize these points (note that this may add new points, if certain flags on the 
+//   tetrahedalization algorithm are set
+// Using a signed distance function, assign a property of "in" or "out" to each point
+// Filter out the "exterior tets" (those with at least on out point)
+// Create faces for the boundary of the remaining tetrahedrons
+// Write an obj file using these faces
+
 #include <iostream>
+#include <sstream>
+
 #include <Eigen/Dense>
 
 #include <igl/opengl/glfw/Viewer.h>
 #include <igl/copyleft/tetgen/tetrahedralize.h>
 #include <igl/barycenter.h>
+#include <igl/writeOBJ.h>
 
 void makeCube( Eigen::MatrixXd& V, Eigen::MatrixXi& F )
 {
@@ -36,20 +51,13 @@ void makeRandPoints( Eigen::MatrixXd& V, Eigen::MatrixXi& F,
                      const Eigen::Vector3d& ptmin, const Eigen::Vector3d& ptmax,
 					 const int npoints )
 {
-    // const int np = 30;
-	// V.resize(np, 3);
-    // V.Random(np, 3);
+	// Range for x, y, z = [-1, 1] in the matrix V
     V = Eigen::MatrixXd::Random(npoints, 3);
 	for ( auto ipt=0; ipt<npoints; ++ipt)
-	    for ( auto i=0; i<3; ++i)
-	        V(ipt, i) = (ptmax(i) - ptmin(i)) * V(ipt, i) + ptmin(i);
-
-	/*
-	F.resize(1, 3);
-	F(0, 0) = 0;
-	F(0, 1) = 1;
-	F(0, 2) = 2;
-	*/
+	    for ( auto i=0; i<3; ++i) {
+			V(ipt, i) = 0.5 * V(ipt, i) + 0.5;                       // Resize to unit cube [0,1] x [0,1] x [0,1]
+	        V(ipt, i) = (ptmax(i)-ptmin(i)) * V(ipt, i) + ptmin(i);  // resize and translate to requested xample rectangle
+		}
 }
 
 void convertDelTets( const Eigen::MatrixXd& TV, const Eigen::MatrixXi& TT,
@@ -99,7 +107,7 @@ void getInteriorTets( const Eigen::MatrixXi& TT, const std::vector<double>& sdli
 void sdist(const Eigen::MatrixXd& V, std::vector<double>& sdlist)
 {
 	auto sdfunc = [](std::vector<double> p) {
-		std::vector<double> center = { -1, 1, 3} ;
+		std::vector<double> center = { -1, 3, 5} ;
 		const double radius_offset = 1;
 		return sqrt( (p[0] - center[0]) * (p[0] - center[0])
 				   + (p[1] - center[1]) * (p[1] - center[1])
@@ -107,15 +115,39 @@ void sdist(const Eigen::MatrixXd& V, std::vector<double>& sdlist)
 			- radius_offset;
 	};
 	int npoints = V.rows();
-	for (unsigned ipt=0; ipt<npoints; ++ipt) {
+	for (auto ipt=0; ipt<npoints; ++ipt) {
 		std::vector<double> p = { V(ipt, 0), V(ipt, 1), V(ipt, 2) };
 	    sdlist.push_back( sdfunc( p) );
     }
 }
 
 
+void usage(const std::string& cmd, const std::string& mess)
+{
+    std::cout << "usage: " << cmd << "npoints fname.obj" << std::endl;
+    std::cout << "exit/error message: " << mess << std::endl;
+	exit(0);
+}
+
 int main( int argc, char *argv[] )
 {
+	if (argc != 3)
+	    usage(argv[0], "too few arguments");
+
+	std::string cmd(argv[1]);
+	
+	int npoints;
+	std::istringstream ss(argv[1]);
+	if (!(ss >> npoints)) {
+	    std::cout << "Invalid number: " << argv[1] << '\n';
+	    usage(cmd, "invalid \"npoints\"");
+	} else if (!ss.eof()) {
+	    std::cout << "Trailing characters after number: " << argv[1] << '\n';
+	    usage(cmd, "invalid \"npoints\"");
+	}
+	
+    std::string fnameOutObj(argv[2]);
+
 	// Tetrahedraliza set of points
 	// Tetrahedralized interior
 	Eigen::MatrixXd TV;
@@ -124,32 +156,12 @@ int main( int argc, char *argv[] )
     Eigen::MatrixXd B;
 
 	// ------------------------------------------------------
-	/*
-    Eigen::MatrixXd V;
-    Eigen::MatrixXi F;
-	// Make a simple cube
-    makeCube( V, F );
-    // Tetrahedralize the interior
-    igl::copyleft::tetgen::tetrahedralize(V,F,"pq1.414Y", TV,TT,TF);
-
-	// Compute barycenters
-	igl::barycenter(TV,TT,B);
-
-    // Plot the mesh
-    igl::opengl::glfw::Viewer viewer;
-	viewer.data().clear();
-    viewer.data().set_mesh(V, F);
-    viewer.data().set_face_based(true);
-    */
-
-	// ------------------------------------------------------
     Eigen::MatrixXd V;
     Eigen::MatrixXi F;
 
 	// Make set of points, but without any faces
-	const int npoints = 100000;
-	const Eigen::Vector3d shapemin(-2,  2, 4);
-	const Eigen::Vector3d shapemax( 0,  4, 6);
+	const Eigen::Vector3d shapemin(-2,  2,  4);
+	const Eigen::Vector3d shapemax( 0,  4,  6);
 	makeRandPoints( V, F, shapemin, shapemax, npoints ); 
 
     // Tetrahedralize the interior
@@ -171,7 +183,10 @@ int main( int argc, char *argv[] )
     // Create faces for all of the tetrahedrons
     Eigen::MatrixXd Vtet;
     Eigen::MatrixXi Ftet;
-    convertDelTets( TV, TTint, Vtet, Ftet );
+    convertDelTets(TV, TTint, Vtet, Ftet);
+
+	// Write an obj file
+    igl::writeOBJ(fnameOutObj, Vtet, Ftet);
 
     // Plot the mesh
     igl::opengl::glfw::Viewer viewer;
