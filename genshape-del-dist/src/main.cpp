8cc090dfaa20d2c6b67b9944c84b6d76998fcf25
Erik Brisson <ebrisson@gmail.com>
2023-08-05 13:49:00 -0700

    checkpoint

--- genshape-del-dist/src/main.cpp
+++ genshape-del-dist/src/main.cpp
@@ -11,6 +11,7 @@
 // Write an obj file using these faces
 
 #include <iostream>
+#include <fstream>
 #include <sstream>
 
 #include <Eigen/Dense>
@@ -44,7 +45,7 @@ void makeCube( Eigen::MatrixXd& V, Eigen::MatrixXi& F )
     0,4,5,
     0,5,1,
     1,5,7,
-    1,7,3; // .finished();
+    1,7,3;
 }
 
 void makeRandPoints( Eigen::MatrixXd& V, Eigen::MatrixXi& F,
@@ -58,6 +59,28 @@ void makeRandPoints( Eigen::MatrixXd& V, Eigen::MatrixXi& F,
 			V(ipt, i) = 0.5 * V(ipt, i) + 0.5;                       // Resize to unit cube [0,1] x [0,1] x [0,1]
 	        V(ipt, i) = (ptmax(i)-ptmin(i)) * V(ipt, i) + ptmin(i);  // resize and translate to requested xample rectangle
 		}
+}
+
+// Add a set of points on or outside of the bounding box,
+// with signed distanc of 1
+void addExteriorPoints( Eigen::MatrixXd& TV, std::vector<double>& sdlist,
+                     const Eigen::Vector3d& ptmin, const Eigen::Vector3d& ptmax)
+{
+	int nptsAdd = 8;
+
+    auto npts = TV.rows();
+	TV.conservativeResize(npts + nptsAdd, 3);
+	int k = 0;
+	for (int ix=0; ix<=1; ++ix)
+		for (int iy=0; iy<=1; ++iy)
+			for (int iz=0; iz<=1; ++iz, ++k) {
+				TV(npts+k, 0) = ix*ptmin[0] + (1-ix)*ptmax[0];
+				TV(npts+k, 1) = iy*ptmin[1] + (1-iy)*ptmax[1];
+				TV(npts+k, 2) = iz*ptmin[2] + (1-iz)*ptmax[2];
+			}
+
+	for (auto i=0; i<nptsAdd; ++i)
+	    sdlist.push_back(1.);
 }
 
 void convertDelTets( const Eigen::MatrixXd& TV, const Eigen::MatrixXi& TT,
@@ -79,6 +102,7 @@ void convertDelTets( const Eigen::MatrixXd& TV, const Eigen::MatrixXi& TT,
 	}
 }
 
+// Just returns "interior" tetrahedrons
 // If the value of sdlist is <= 0, it's corresponding point is on the
 // interior or surface of the implicit object
 // We filter out all tets whose verts are all exterior, i.e., whose sdlist > 0
@@ -97,13 +121,54 @@ void getInteriorTets( const Eigen::MatrixXi& TT, const std::vector<double>& sdli
 		if (!ext)
 	        ttInteriorInd.push_back(itet);
     }
+
 	int ntetsInterior = ttInteriorInd.size();
 	TTint.resize(ntetsInterior, 4);
-	for (unsigned itet=0; itet<ntetsInterior; ++itet)
-	    for (int i=0; i<4; ++i)
+	for (auto itet=0; itet<ntetsInterior; ++itet)
+	    for (auto i=0; i<4; ++i)
 	        TTint(itet, i) = TT(ttInteriorInd[itet], i);
 }
 
+// Intent is to return the boundary of a set of tetrahedra
+// If a tet has one exterior vert and three interior verts, we declare
+// that those three verts form a boundary triangle (note that this
+// may allow for the same boundary triangle to appear twice, once in
+// each orientation).  Note that the %4 is needed to get boundary
+// triangle correct
+// If the value of sdlist is <= 0, it's corresponding point is on the
+// interior or surface of the implicit object
+void getBoundary( const Eigen::MatrixXi& TT, const std::vector<double>& sdlist, Eigen::MatrixXi& Ftri)
+{
+	std::vector<std::array<int, 3>> triBound;
+	int ntets = TT.rows();
+	for (auto itet=0; itet<ntets; ++itet) {
+	    int next = 0;
+		int iext = -1;
+		for (auto i=0; i<4; ++i)
+		    if (sdlist[TT(itet, i)] > 0 ) {
+			    next++;
+				iext = i;
+			}
+		if (iext == 1)
+	        triBound.push_back( { TT(itet, (iext+1)%4), TT(itet, (iext+2)%4), TT(itet, (iext+3)%4) } );
+	}
+
+	auto nfaces = triBound.size();
+	Ftri.resize(nfaces, 3);
+	for (auto iface=0; iface<nfaces; ++iface)
+	    for (auto i=0; i<3; ++i)
+	        Ftri(iface, i) = triBound[iface][i];
+   
+}
+
+// Convert eigen matrix of triangles to a vector of general size faces
+void triToFace( const Eigen::MatrixXi& Ftri, std::vector<std::vector<int>>& faces)
+{
+    for (auto itri=0; itri<Ftri.rows(); ++itri)
+	    faces.push_back( { Ftri(itri, 0), Ftri(itri, 1), Ftri(itri, 2) } );
+}
+
+// Signed distance function
 void sdist(const Eigen::MatrixXd& V, std::vector<double>& sdlist)
 {
 	auto sdfunc = [](std::vector<double> p) {
@@ -121,7 +186,22 @@ void sdist(const Eigen::MatrixXd& V, std::vector<double>& sdlist)
     }
 }
 
+// -----------------------------------------------------------------------------------
+void ebWriteOBJ(const std::string& fname, const Eigen::MatrixXd& Vtet, const std::vector<std::vector<int>>& faces)
+{
+	std::ofstream myfile;
+	myfile.open (fname);
+	for (auto ipt=0; ipt<Vtet.rows(); ++ipt)
+	    myfile << "v " << Vtet(ipt, 0) << " " << Vtet(ipt, 1) << " " << Vtet(ipt, 2) << std::endl;
+	for (auto face : faces) {
+	    myfile << "f";
+	    for (auto ipt : face) 
+		    myfile << " " << ipt;
+	    myfile << std::endl;
+	}
+}
 
+// -----------------------------------------------------------------------------------
 void usage(const std::string& cmd, const std::string& mess)
 {
     std::cout << "usage: " << cmd << "npoints fname.obj" << std::endl;
@@ -148,23 +228,17 @@ int main( int argc, char *argv[] )
 	
     std::string fnameOutObj(argv[2]);
 
-	// Tetrahedraliza set of points
-	// Tetrahedralized interior
-	Eigen::MatrixXd TV;
-	Eigen::MatrixXi TT;
-	Eigen::MatrixXi TF;
-    Eigen::MatrixXd B;
-
-	// ------------------------------------------------------
+	// Make set of points, but without any faces
     Eigen::MatrixXd V;
     Eigen::MatrixXi F;
-
-	// Make set of points, but without any faces
 	const Eigen::Vector3d shapemin(-2,  2,  4);
 	const Eigen::Vector3d shapemax( 0,  4,  6);
 	makeRandPoints( V, F, shapemin, shapemax, npoints ); 
 
     // Tetrahedralize the interior
+	Eigen::MatrixXd TV;
+	Eigen::MatrixXi TT;
+	Eigen::MatrixXi TF;
     igl::copyleft::tetgen::tetrahedralize(V,F,"", TV,TT,TF);
     std::cout << "Number of tets in TT = " << TT.rows() << std::endl;
 
@@ -172,26 +246,47 @@ int main( int argc, char *argv[] )
     std::vector<double> sdlist;
     sdist(TV, sdlist);
 
+    // Add explicit exterior points
+	addExteriorPoints( TV, sdlist, shapemin, shapemax);
+
+	/*
 	// Filter out the "exterior tets"
 	Eigen::MatrixXi TTint;
     getInteriorTets(TT, sdlist, TTint);
     std::cout << "Number of tets in TTint = " << TTint.rows() << std::endl;
 
 	// Compute barycenters
+    // Eigen::MatrixXd B;
 	// igl::barycenter(TV,TT,B);
 
     // Create faces for all of the tetrahedrons
     Eigen::MatrixXd Vtet;
     Eigen::MatrixXi Ftet;
     convertDelTets(TV, TTint, Vtet, Ftet);
+    */
+
+	// Get a list of triangles representing the "boundary" of the "interior" tets
+	Eigen::MatrixXi FtriBound;
+	getBoundary( TT, sdlist, FtriBound);
+
+    // Convert triangle matrix to general face form
+	std::vector<std::vector<int>> boundary;
+	triToFace(FtriBound, boundary);
+
+	std::cout << "FtriBound.rows() = " << FtriBound.rows() << std::endl;
+	std::cout << "boundary.size() = " << boundary.size() << std::endl;
+
+	// Write an obj file using my own routine
+    ebWriteOBJ(fnameOutObj, TV, boundary);
 
 	// Write an obj file
-    igl::writeOBJ(fnameOutObj, Vtet, Ftet);
+    // igl::writeOBJ(fnameOutObj, Vtet, Ftet);
 
     // Plot the mesh
     igl::opengl::glfw::Viewer viewer;
 	viewer.data().clear();
-    viewer.data().set_mesh(Vtet, Ftet);
+    // viewer.data().set_mesh(Vtet, Ftet);
+    viewer.data().set_mesh(TV, FtriBound);
     viewer.data().set_face_based(true);
     viewer.launch();
 }
